251d11914242a3a42523bbf53a8c137534f2a645
Ger Lpz <german.lopez.130194@gmail.com>
2022-07-15 19:29:10 -0400

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app =express()
 app.use(express.json())
 dotenv.config()
 conectarDB()
-const dominiosPermitidos =['http://localhost:3000']
+const dominiosPermitidos =[process.env.FRONTEND_URL]
 
 const corsOptions={
     origin: function (origin, callback){
